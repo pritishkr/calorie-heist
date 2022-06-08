c5dac8ccc7525f552391c604e1277f40816d9856
pritishkr <pritish.kr3@gmail.com>
2022-06-08 14:17:44 +0530

    commit@8th june 2:20pm

--- server/index.js
+++ server/index.js
@@ -5,9 +5,10 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+
+// Available routes
+app.use('/api/auth', require('./routes/auth'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ server/routes/auth.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const User = require('../models/User');
+const router = express.Router();
+
+
+
+// Create a user using: POST "/api/auth/".  Doesn't require auth
+router.post('/',(req,res)=>{
+    console.log(req.body);
+    const user = User(req.body);
+    user.save()
+    res.send("Hello");
+})
+
+
+
+
+
+module.exports = router
